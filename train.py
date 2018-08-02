4b196006fc58eb2b329122ce2efc5678e903cd6c
XuJun <nankaimathxujun@gmail.com>
2018-08-02 11:40:08 -0700

    add

--- train.py
+++ train.py
@@ -1328,7 +1328,7 @@ def test(options):
                 statistics = visualizeBatch(options, 'test_' + str(iteration), pred,
                                             {'corner': gt['corner'], 'icon': gt['icon'], 'room': gt['room'],
                                              'density': debug['x0_topdown'][:, :, :, -1],
-                                             'image_path': inp['image_path']}, dataset, savePredictions=False)
+                                             'image_path': inp['image_path']}, dataset)
 
                 for k, v in statistics.iteritems():
                     if k in statisticsSum:
@@ -1373,7 +1373,7 @@ def test(options):
     return numbers
 
 
-def visualizeBatch(options, prefix, pred_dict, gt_dict, datasetFlag, savePredictions=False):
+def visualizeBatch(options, prefix, pred_dict, gt_dict, datasetFlag):
     # To update the modified time for options.test_dir
     if os.path.exists(options.test_dir + '/dummy'):
         # os.rmdir(options.test_dir + '/dummy')
@@ -1381,7 +1381,7 @@ def visualizeBatch(options, prefix, pred_dict, gt_dict, datasetFlag, savePredict
     else:
         os.mkdir(options.test_dir + '/dummy')
         pass
-    if savePredictions:
+    if options.savePredictions:
         print('save')
         np.save(options.test_dir + '/dummy/gt_dict.npy', gt_dict)
         np.save(options.test_dir + '/dummy/pred_dict.npy', pred_dict)
@@ -1660,6 +1660,9 @@ def parse_args():
     parser.add_argument('--evaluateImage', dest='evaluateImage',
                         help='evaluate image',
                         action='store_true')
+    parser.add_argument('--savePredictions', dest='savePredictions',
+                        help='save predictions',
+                        default=False, type=bool)
 
     args = parser.parse_args()
 
